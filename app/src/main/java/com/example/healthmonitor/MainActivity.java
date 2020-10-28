02614eb0a2d698eecb1537239b9dbe0b3d798b63
mindyp <pmmindy@qq.com>
2020-10-29 01:39:21 +0800

    init commit

---
+++ app/src/main/java/com/example/healthmonitor/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.healthmonitor;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private Fragment homeFragment;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home: {
+                    initFragment(0);
+                    return true;
+                }
+
+                case R.id.navigation_dashboard:
+
+                    return true;
+                case R.id.navigation_notifications:
+
+                    return true;
+                case R.id.navigation_settings:
+
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
+    private void initFragment(int index) {
+        // 由于是引用了V4包下的Fragment，所以这里的管理器要用getSupportFragmentManager获取
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        // 开启事务
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        // 隐藏所有Fragment
+        switch (index) {
+            case 0:
+                if (homeFragment == null) {
+                    homeFragment = new HomeFragment();
+                    transaction.add(R.id.fl_content, homeFragment);
+                } else {
+                    transaction.show(homeFragment);
+                }
+                break;
+        }// 提交事务
+        transaction.commit();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        initFragment(0);
+    }
+
+}
